13c138ece6f2707a4a8f204bec95745cfe3f8ced
NutellaGang <90981495+NutellaGang@users.noreply.github.com>
2025-03-27 22:40:45 +0300

    Update homework.php

--- homework.php
+++ homework.php
@@ -1,10 +1,28 @@
 <?php
 
-
+echo "Введите имя: ";
 $name1 = trim(fgets(STDIN));
-$name2 =  trim(fgets(STDIN));
-$name3 =  trim(fgets(STDIN));
 
-echo $name1;
-echo $name2;
-echo $name3;
+echo "Введите фамилию: ";
+$name2 = trim(fgets(STDIN));
+
+echo "Введите отчество: ";
+$name3 = trim(fgets(STDIN));
+
+$name1 = mb_convert_case($name1, MB_CASE_TITLE, "UTF-8");
+$name2 = mb_convert_case($name2, MB_CASE_TITLE, "UTF-8");
+$name3 = mb_convert_case($name3, MB_CASE_TITLE, "UTF-8");
+
+$fullname = $name2 . " " . $name1 . " " . $name3;
+
+$initial1 = mb_substr($name1, 0, 1);
+$initial2 = mb_substr($name2, 0, 1);
+$initial3 = mb_substr($name3, 0, 1);
+
+$surnameAndInitials = $name2 . " " . $initial1 . "." . $initial3 . ".";
+
+$fio = $initial2 . $initial1 . $initial3;
+
+echo "Полное имя: '$fullname'". PHP_EOL;
+echo "Фамилия и инициалы: '$surnameAndInitials'" . PHP_EOL;
+echo "Аббревиатура: '$fio'" . PHP_EOL;
